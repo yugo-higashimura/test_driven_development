2f72fb41c800fae5c554a92d6b2a030abe75c8d4
Yugo Higashimura <yugo.higashimura@satori.team>
2024-07-28 11:28:35 +0000

    Refactor - Generalization of the equal method

--- src/money/Franc.java
+++ src/money/Franc.java
@@ -1,18 +1,11 @@
 package money;
 
-class Franc {
-  private int amount;
-
+class Franc extends Money {
   Franc(int amount) {
     this.amount = amount;
   }
 
   Franc times(int multiplier) {
     return new Franc(amount * multiplier);
-  }
-
-  public boolean equals(Object object) {
-    Franc Franc = (Franc) object;
-    return amount == Franc.amount;
   }
 }
