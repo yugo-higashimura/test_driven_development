6972536eeae740a001dbf5e6e00924c72af289a6
Yugo Higashimura <yugo.higashimura@satori.team>
2024-08-04 01:02:40 +0000

    Refactor - Add toString method

--- src/money/Money.java
+++ src/money/Money.java
@@ -18,7 +18,11 @@ public class Money implements Expression {
   }
 
   Expression plus(Money addend) {
-    return new Money(amount + addend.amount, currency);
+    return new Sum(this, addend);
+  }
+
+  public String toString() {
+    return amount + " " + currency;
   }
 
   public boolean equals(Object object) {
